8daeb24aebbf8bd9da97342c9e473f5c2301dd71
llll12t <147978645+llll12t@users.noreply.github.com>
2024-01-23 14:01:34 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAy9Nvu3357kNsOd9EMB-vrw9fqSqMuAQs",
+  authDomain: "iiirnstudio-6627b.firebaseapp.com",
+  projectId: "iiirnstudio-6627b",
+  storageBucket: "iiirnstudio-6627b.appspot.com",
+  messagingSenderId: "934458269500",
+  appId: "1:934458269500:web:544e288fbc58e958834839"
+};
+
+firebase.initializeApp(firebaseConfig);
